97330b612f4cf21a4a23e231a24fe7d64b59d93a
Rasheed-Ahamed <78206567+Rasheed-Ahamed@users.noreply.github.com>
2021-07-23 12:20:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+Webcam.attach(  '#camera'  );
+camera = document.getElementById("camera");
+
 Webcam.set({
     width:350,
     height:300,
@@ -5,15 +8,10 @@ Webcam.set({
     jpeg_quality:90
 });
 
-camera = document.getElementById("camera");
-
-
-Webcam.attach(  '#camera'  );
-
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-      document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
